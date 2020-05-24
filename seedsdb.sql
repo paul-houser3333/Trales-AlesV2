88db010d0c867bca32faa85f1299d742f7763d5c
Mike Houser <saintmikel2004@gmail.com>
2020-05-23 20:15:34 -0400

    ()

--- seedsdb.sql
+++ seedsdb.sql
@@ -10,60 +10,60 @@
 -- 	CONSTRAINT trail_id_pk PRIMARY KEY
 --     (trail_id)
 
-INSERT INTO trail
-    (api_id, trail_name, difficulty, longitude, latitude)
+INSERT INTO trail;
+    (api_id, trail_name, difficulty, longitude, latitude);
 VALUES
-(7070379, Old Mill Trail- Beaver Lake Trail, blue, -77.582, 37.386);
-(7071477, Fendley Station Loop A, blue, -77.5762, 37.3895);
-(7052134, Cold Harbor Battlefield, blue, -77.2868, 37.5855);
-(7063874, James River Loop, black, -77.4825, 37.535);
-(7026682, Appomattox River Park Loop, blue, -77.3385, 37.3097);
-(7070375, Bright Hope Trail and Otter Lick Loop, blue, -77.5853, 37.3596);
-(7024114, Dutch Gap Run, blue, -77.3622, 37.3739;)
-(7026528, Mid-Lothian Mines, greenBlue, -77.6437, 37.4961);
-(7070402, Beaver Lake Trail, blue, -77.5824, 37.3866;
-(7070403, Forest Exploration Trail, blue, -77.5703, 37.388);
-(7029046, Green Trail, black, -77.3385, 37.3097);
-(7029042, Red Trail, black, -77.3385, 37.3097);
-(7026522, Red Square, blue, -77.6436, 37.4934);
-(7026520, White Rectangle, blue, -77.6434, 37.4958);
-(7011872, Forrest Hill Park Loop, blue, -77.4701, 37.523);
-(7011875, Buttermilk Trail, blueBlack, -77.4855, 37.5294);
-(7011871,  Belle Isle,  greenBlue, -77.4483, 37.5348);
-(7070691,  Fendley Station Trail, blue, -77.5405, 37.3845);
-(7012245,  Ancarrow's Landing, -77.4188, 37.5193);
-(7011873, North Bank Trail, greenBlue, -77.4826, 37.5351);
-(7014388, Blue Trail, blue, -77.5799, 37.4487);
-(7070470,   Otter Lick Loop, greenBlue, -77.5693, 37.3474);
-(7024094,  White Blaze Trail,  blue, -77.3681, 37.3677);
-(7026693, Yellow Trail,  blue, -77.3385, 37.3097);
-(7092909,  Big Poplar Trail, blue, -77.5829, 37.3884);
-(7014462, Pony Pasture Main Loop, greenBlue, -77.5217, 37.5518);
-(7063964, T. Tyler Potterfield Memorial Walkway, green, -77.447, 37.5285);
-(7070469,  Bright Hope Trail, green, -77.5853, 37.3596);
-(7012244, Richmond Slave Trail, green, -77.4351, 37.5269);
-(7070496, Old Mill Trail, green, -77.582, 37.3865);
-(7018586, Pony Pasture Wetland Trails, greenBlue, -77.5111, 37.5473);
-(7071805, Deerfield Forest Trail, green, -77.5519, 37.3991);
-(7052141,  Main Trail, green, -77.2868, 37.5855);
-(7071804,  Fendley Station Loop A, green, -77.5362, 37.3973;)
-(7052145, Western Trail, green, -77.2903, 37.5895);
-(7052142, Extended Loop Trail, green, -77.2877, 37.5888);
-(7070497,  Old Mill to Beaver Lake Connector, blue, -77.5935, 37.3805);
-(7011874, Dogwood Dell Loop, blue, -77.4857, 37.5372);
-(7015719, Orange Trail,  blue, -77.5801, 37.4526);
-(7017893, East\/West Trail, blue, -77.5602, 37.5511);
-(7017894, North\/South Trail, blue, -77.5621, 37.5379);
-(7029047, Perimeter Trail, black, -77.3438, 37.3137);
-(7024113, Blue Blaze Trail, blue, -77.3626, 37.3737,);
-(7026527, Orange Circle Trail,  blue, -77.6434, 37.4876);
-(7024111, Red Blaze Trail, blue, -77.3722, 37.3677); 
-(7026525, Yellow Diamond Trail, blue, -77.6424, 37.4951); 
-(7029048,  Cameron's Landing, black, -77.3385, 37.3097);
-(7024102, Dog Leg Point Spur Trail, blue, -77.374, 37.3639);
-(7026526,  Blue Rectangle, green, -77.6422, 37.4953);
-(7024101, Long Point Spur Trail, blue, -77.3844, 37.3688);
-(7024112, Sycamore Spur Trail, blue, -77.364, 37.3716);
-(7024109,  Horsetail Loop Trail, blue, -77.3767, 37.3703);
-(7024106, Oxbow Point Spur Trail, blue, -77.3897, 37.3656);
-(7029392, Yellow Trail Connector,  green, -77.3391, 37.3103);   
+(7070379, (Old Mill Trail- Beaver Lake Trail), (blue), -77.582, 37.386);
+(7071477, (Fendley Station Loop A), (blue, -77.5762, 37.3895);
+(7052134, (Cold Harbor Battlefield), (blue), -77.2868, 37.5855);
+(7063874, (James River Loop), (black, -77.4825, 37.535);
+(7026682, (Appomattox River Park Loop), (blue), -77.3385, 37.3097);
+(7070375, (Bright Hope Trail and Otter Lick Loop), (blue), -77.5853, 37.3596);
+(7024114, (Dutch Gap Run), (blue), -77.3622, 37.3739;)
+(7026528, (Mid-Lothian Mines), (greenBlue), -77.6437, 37.4961);
+(7070402, (Beaver Lake Trail), (blue), -77.5824, 37.3866;
+(7070403, (Forest Exploration Trail), (blue), -77.5703, 37.388);
+(7029046, (Green Trail), (black), -77.3385, 37.3097);
+(7029042, (Red Trail), (black), -77.3385, 37.3097);
+(7026522, (Red Square), (blue), -77.6436, 37.4934);
+(7026520, (White Rectangle), (blue), -77.6434, 37.4958);
+(7011872, (Forrest Hill Park Loop), (blue), -77.4701, 37.523);
+(7011875, (Buttermilk Trail), (blueBlack), -77.4855, 37.5294);
+(7011871, ( Belle Isle,  (greenBlue), -77.4483, 37.5348);
+(7070691, ( Fendley Station Trail), (blue), -77.5405, 37.3845);
+(7012245,  (Ancarrows Landing), (green),-77.4188, 37.5193);
+(7011873, (North Bank Trail), (greenBlue), -77.4826, 37.5351);
+(7014388, (Blue Trail, (blue), -77.5799, 37.4487);
+(7070470,   (Otter Lick Loop), (greenBlue), -77.5693, 37.3474);
+(7024094,  (White Blaze Trail),  (blue), -77.3681, 37.3677);
+(7026693, (Yellow Trail),  (blue), -77.3385, 37.3097);
+(7092909,  (Big Poplar Trail), (blue), -77.5829, 37.3884);
+(7014462, (Pony Pasture Main Loop), (greenBlue), -77.5217, 37.5518);
+(7063964, (T. Tyler Potterfield Memorial Walkway),( green), -77.447, 37.5285);
+(7070469,  (Bright Hope Trail,) (green), -77.5853, 37.3596);
+(7012244, (Richmond Slave Trail, (green, -77.4351, 37.5269);
+(7070496, (Old Mill Trail, (green, -77.582, 37.3865);
+(7018586, (Pony Pasture Wetland Trails, (greenBlue, -77.5111, 37.5473);
+(7071805, (Deerfield Forest Trail, (green, -77.5519, 37.3991);
+(7052141,  (Main Trail, (green, -77.2868, 37.5855);
+(7071804,  (Fendley Station Loop A),(green), -77.5362, 37.3973;)
+(7052145, (Western Trail), (green), -77.2903, 37.5895);
+(7052142, (Extended Loop Trail), (green), -77.2877, 37.5888);
+(7070497,  (Old Mill to Beaver Lake Connector), (blue), -77.5935, 37.3805);
+(7011874, (Dogwood Dell Loop), (blue), -77.4857, 37.5372);
+(7015719, (Orange Trail),  (blue), -77.5801, 37.4526);
+(7017893, (East\/West Trail), (blue), -77.5602, 37.5511);
+(7017894, (North\/South Trail), (blue), -77.5621, 37.5379);
+(7029047, (Perimeter Trail), (black), -77.3438, 37.3137);
+(7024113, (Blue Blaze Trail), (blue), -77.3626, 37.3737,);
+(7026527, (Orange Circle Trail),  (blue), -77.6434, 37.4876);
+(7024111, (Red Blaze Trail), (blue), -77.3722, 37.3677); 
+(7026525, (Yellow Diamond Trail), (blue), -77.6424, 37.4951); 
+(7029048, ( Camerons Landing), (black), -77.3385, 37.3097);
+(7024102, (Dog Leg Point Spur Trail), (blue), -77.374, 37.3639);
+(7026526,  (Blue Rectangle), (green), -77.6422, 37.4953);
+(7024101, (Long Point Spur Trail), (blue, -77.3844, 37.3688);
+(7024112, (Sycamore Spur Trail), (blue), -77.364, 37.3716);
+(7024109,  (Horsetail Loop Trail), (blue), -77.3767, 37.3703);
+(7024106, (Oxbow Point Spur Trail), (blue), -77.3897, 37.3656);
+(7029392, (Yellow Trail Connector), (green), -77.3391, 37.3103);   
