e829b23d0939bf9a4c48dcc69307ebf5b645503c
Daniel Abell <da450@cairn.edu>
2020-05-25 15:14:32 -0400

    fixed more click event bugs

--- public/js/user/trail-search.js
+++ public/js/user/trail-search.js
@@ -120,12 +120,12 @@ $(document).ready(function () {
                     // let trailLat = selectedTrail.latitude;
                     // let trailLon = selectedTrail.longitude;
 
-                    $("body").off().on("click", "button#add-trail", event => {
+                    $("button#add-trail").off().on("click", event => {
                         event.preventDefault();
-                        let apiId = $("button").data("id");
-                        let trailName = $("button").data("name");
-                        let trailLat = $("button").data("lat");
-                        let trailLon = $("button").data("lon");
+                        let apiId = $("button#add-trail").data("id");
+                        let trailName = $("button#add-trail").data("name");
+                        let trailLat = $("button#add-trail").data("lat");
+                        let trailLon = $("button#add-trail").data("lon");
                         console.log(apiId, trailName, trailLat, trailLon);
                         addTrail(apiId, trailName, trailLat, trailLon);
                     });
